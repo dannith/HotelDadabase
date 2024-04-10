d127860cfa1d6c78e8f2711932cd60fcd09665d1
dannith <dannitorr@gmail.com>
2024-04-10 22:01:35 +0000

    data layer fyrir hotel og herbergi Skilar þvi sem lista af hotelum sem java class sem og hvert og eitt hefur lista af herbergjum

--- untitled/src/main/java/org/example/App.java
+++ untitled/src/main/java/org/example/App.java
@@ -1,16 +1,10 @@
 package org.example;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.PreparedStatement;
 import java.util.List;
 
 public class App
 {
     public static void main( String[] args ){
-        List<Hotel> myHotels = null;
+        List<Hotel> myHotels;
         myHotels = HotelDAL.getHotelsFromLocation("Stykkishólmur");
         for(Hotel hotel : myHotels) {
             hotel.setRooms(HotelDAL.getHotelRooms(hotel));
